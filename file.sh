e0d9f206fc8c2279d940ff1c4d51475a21792cf8
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-09-08 11:41:53 +0530

    Delete file.sh

--- file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-__install_ant() {
-  echo "installing ant"
-  curl -sL https://downloads.apache.org//ant/binaries/apache-ant-1.10.11-bin.tar.gz | tar -xz
-  apache-ant-1.10.11/bin/ant
-}
-__install_ant
